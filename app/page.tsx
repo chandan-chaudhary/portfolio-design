7c40bc0f041311397805b85ac0b6704980d2851d
chandan-chaudhary <chandanchaudhary3949@gmail.com>
2024-10-29 09:36:52 +0530

    updated ui structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import About from "./_components/About";
+// import About from "./_components/About";
 import LandingPage from "./_components/LandingPage";
-import Skills from "./_components/Skills";
+// import Skills from "./_components/Skills";
 
 export default function Home() {
   return (
